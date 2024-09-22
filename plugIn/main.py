1be1643c7c258298cfafc9b710c5c96637031328
Rushikesh Nakhate <rushikeshnkahate@gmail.com>
2024-09-22 16:55:00 +0800

    refactor for proper steps

--- plugIn/main.py
+++ plugIn/main.py
@@ -22,16 +22,21 @@ logger = logging.getLogger(__name__)
 output_dir = Path(r"D:\PortfoliOpt\data")
 pkl_filepath = os.path.join(output_dir, "results.pkl")
 allocation_pkl_filepath = os.path.join(output_dir, "allocation.pkl")
+performance_pkl_filepath = os.path.join(output_dir, "performance.pkl")
 
 if __name__ == "__main__":
     data = get_stocks()
     expected_return_df = calculate_all_returns(data)
     risk_return_dict = calculate_all_risk_matrix(data)
     results_df = calculate_optimizations(data, expected_return_df, risk_return_dict)
+
     clean_up(results_df)
     results_df = run_monte_carlo_simulation(data, results_df)
     results_df.to_pickle(pkl_filepath)
+
     allocation_df = run_all_post_processing_weight(results_df, data)
-    calculate_performance(allocation_df, data, '2024-01-01', '2024-07-29')
-    print(tabulate(allocation_df, headers='keys', tablefmt='grid'))
     allocation_df.to_pickle(allocation_pkl_filepath)
+
+    performance_df = calculate_performance(allocation_df, data, '2024-01-01', '2024-07-29')
+    print(tabulate(performance_df, headers='keys', tablefmt='grid'))
+    performance_df.to_pickle(performance_pkl_filepath)
