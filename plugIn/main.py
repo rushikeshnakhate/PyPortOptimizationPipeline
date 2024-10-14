889d92353449770ab9ec859506c17d7ed874390f
Rushikesh <rushikeshnakhate@gmail.com>
2024-10-14 19:46:48 +0800

    downloader classes for diff assests class

--- plugIn/main.py
+++ plugIn/main.py
@@ -1,21 +1,18 @@
 import logging
 from pathlib import Path
-import cProfile
-import pandas as pd
-import pstats
 
-from tabulate import tabulate
+import pandas as pd
 
 from plugIn.common.execution_time_recorder import ExecutionTimeRecorder
 from plugIn.common.logging_config import setup_logging
+from plugIn.common.utils import generate_month_date_ranges, create_current_month_directory
+from plugIn.dataDownloader.get_stocks import get_stocks
 from plugIn.expected_return.main import calculate_or_get_all_return
 from plugIn.experimental.monte_carlo_simulation import run_monte_carlo_simulation
-from plugIn.dataDownloader.get_stocks import get_stocks
 from plugIn.optimization.main import calculate_optimizations
 from plugIn.performance.main import calculate_performance
 from plugIn.processing_weight.main import run_all_post_processing_weight
 from plugIn.risk_returns.main import calculate_all_risk_matrix
-from plugIn.common.utils import generate_month_date_ranges, create_current_month_directory
 
 pd.set_option('display.max_colwidth', None)  # Display full content in cells
 
@@ -37,11 +34,11 @@ if __name__ == "__main__":
         expected_return_df = calculate_or_get_all_return(data, current_month_dir)
         risk_return_dict = calculate_all_risk_matrix(data, current_month_dir)
         optimized_df = calculate_optimizations(data, expected_return_df.head(10), risk_return_dict, current_month_dir)
-        # monte_carlo_df = run_monte_carlo_simulation(output_dir, data)
-        # all_optimized_df = pd.concat([monte_carlo_df, optimized_df], ignore_index=True)
-        # post_processing_wright_df = run_all_post_processing_weight(optimized_df, data, current_month_dir)
-        # performance_df = calculate_performance(post_processing_wright_df, data, start_date, end_date,
-        #                                        current_month_dir)
+        monte_carlo_df = run_monte_carlo_simulation(output_dir, data)
+        all_optimized_df = pd.concat([monte_carlo_df, optimized_df], ignore_index=True)
+        post_processing_wright_df = run_all_post_processing_weight(optimized_df, data, current_month_dir)
+        performance_df = calculate_performance(post_processing_wright_df, data, start_date, end_date,
+                                               current_month_dir)
         # print(tabulate(optimized_df.head(20), headers='keys', tablefmt='pretty'))
 
         ExecutionTimeRecorder.print_results()
