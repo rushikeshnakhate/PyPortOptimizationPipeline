9be06fcb8bb572055504aa987946f9a76fe1ac47
Rushikesh Nakhate <rushikeshnkahate@gmail.com>
2024-11-03 11:28:22 +0800

    MAIN YEAR SUPPORT

--- plugIn/test_main.py
+++ plugIn/test_main.py
@@ -8,7 +8,7 @@ from tabulate import tabulate
 from plugIn.common.execution_time_recorder import ExecutionTimeRecorder
 from plugIn.common.hydra_config_loader import load_config
 from plugIn.common.logging_config import setup_logging
-from plugIn.common.utils import generate_month_date_ranges, create_current_month_directory
+from plugIn.common.utils import generate_month_date_ranges, create_current_month_directory, generate_date_ranges
 from plugIn.dataDownloader.main import get_data
 
 from plugIn.expected_return.main import calculate_or_get_all_return
@@ -29,21 +29,21 @@ logging.basicConfig(level=logging.INFO)
 
 @ExecutionTimeRecorder(module_name=__name__)
 def main():
-    month_ranges = generate_month_date_ranges(configuration.year, months=configuration.months)
-    for start_date, end_date in month_ranges:
-        current_month_dir = create_current_month_directory(start_date, configuration.output_dir)
+    date_ranges = generate_date_ranges(year=configuration.year, months=configuration.months, frequency="yearly")
+    for start_date, end_date in date_ranges:
+        current_dir = create_current_month_directory(start_date, configuration.output_dir)
         logger.info(f"Processing start_date={start_date}, end_date={end_date} "
-                    f"for current_month_dir={current_month_dir}")
-        data = get_data(current_dir=current_month_dir, start_date=start_date, end_date=end_date)
-        expected_return_df = calculate_or_get_all_return(data, current_month_dir)
-        risk_return_dict = calculate_all_risk_matrix(data, current_month_dir)
-        optimized_df = calculate_optimizations(data, expected_return_df.head(10), risk_return_dict, current_month_dir)
+                    f"for current_dir={current_dir}")
+        data = get_data(current_dir=current_dir, start_date=start_date, end_date=end_date)
+        expected_return_df = calculate_or_get_all_return(data, current_dir)
+        risk_return_dict = calculate_all_risk_matrix(data, current_dir)
+        optimized_df = calculate_optimizations(data, expected_return_df.head(10), risk_return_dict, current_dir)
         monte_carlo_df = run_monte_carlo_simulation(configuration.output_dir, data)
         all_optimized_df = pd.concat([monte_carlo_df, optimized_df], ignore_index=True)
-        save_pickle = Path(current_month_dir) / 'all_optimized_df.pkl'
+        save_pickle = Path(current_dir) / 'all_optimized_df.pkl'
         all_optimized_df.to_pickle(save_pickle)
-        # post_processing_wright_df = run_all_post_processing_weight(all_optimized_df, data, current_month_dir)
-        # performance_df = calculate_performance(post_processing_wright_df, data, start_date, end_date, current_month_dir)
+        # post_processing_wright_df = run_all_post_processing_weight(all_optimized_df, data, current_dir)
+        # performance_df = calculate_performance(post_processing_wright_df, data, start_date, end_date, current_dir)
 
 
 if __name__ == "__main__":
